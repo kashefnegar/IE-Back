ae6dbe50cf6b33ab7a1cbf3014eae91f03167d9b
Negar <kashefnegar@gmail.com>
2019-05-06 22:48:47 +0430

    Started SignUp API

--- src/main/java/servlet/mytools/user_page_tool.java
+++ src/main/java/servlet/mytools/user_page_tool.java
@@ -1,4 +1,12 @@
 package servlet.mytools;
 
+import models.MyUser;
+
 public class user_page_tool {
+    private MyUser users;
+    private int state;
+
+    public void UserPageTool(String id){
+        if()
+    }
 }
