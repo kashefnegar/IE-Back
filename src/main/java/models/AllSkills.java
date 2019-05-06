841bca04c0f9d35ffee42bd93e22f90b3a72cfd7
Negar <kashefnegar@gmail.com>
2019-05-06 17:57:33 +0430

    Connection Checking

--- src/main/java/models/AllSkills.java
+++ src/main/java/models/AllSkills.java
@@ -1,8 +1,9 @@
 package models;
 
-import datalayer.ConnectionPool;
-import datalayer.dbConnection.BasicDBConnectionPool;
-import datalayer.dbConnection.impl.SQLiteBasicDBConnectionPool;
+//import datalayer.dbConnection.BasicDBConnectionPool;
+//import datalayer.dbConnection.impl.SQLiteBasicDBConnectionPool;
+
+import datalayer.HikariCPDataSource;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -27,8 +28,7 @@ public class AllSkills {
             allskills = new Utility().getskilsnames( new Utility().jsonbufferstring(get_skills.getContent()));
 
             if(!cheaking_skillexit()) {
-                SQLiteBasicDBConnectionPool sqlInstance = ConnectionPool.getInstance();
-                Connection conn = sqlInstance.get();
+                Connection conn = HikariCPDataSource.getConnection();
                 for (int i = 0; i < allskills.size(); i++) {
                     try {
                         Statement stmt = conn.createStatement();
@@ -36,14 +36,16 @@ public class AllSkills {
                     } catch (SQLException e) {
                         e.printStackTrace();
                     }
-//                connection.get().commit("INSERT INTO Skill VALUES("+String(i) +"," + allskills.get(i).getName()+")");
                 }
-                sqlInstance.release(conn);
             }
 
 
 
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
 
@@ -57,23 +59,42 @@ public class AllSkills {
 
     private AllSkills() {
     }
-    public boolean cheaking_skillexit(){
-        SQLiteBasicDBConnectionPool sqlInstance= ConnectionPool.getInstance();
-        Connection conn = sqlInstance.get();
-        try {
-            Statement stmt =conn.createStatement();
-            ResultSet rs =stmt.executeQuery("select count (*) FROM Skill ");
-            sqlInstance.release(conn);
-            if (rs.toString().equals("0")){
-                return false;
+    public boolean cheaking_skillexit() throws SQLException, ClassNotFoundException {
+//        Connection conn = HikariCPDataSource.getConnection();
+//        try {
+//            Statement stmt = conn.createStatement();
+//            ResultSet rs =stmt.executeQuery("select count (*) FROM Skill ");
+//
+//            if (rs.toString().equals("0")){
+//                return false;
+//            }
+//            else {
+//                return true;
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+        String SQL_QUERY = "select count (*) FROM Skill";
+//        List<Employee> employees = null;
+        try (Connection con = HikariCPDataSource.getConnection();
+             PreparedStatement pst = con.prepareStatement( SQL_QUERY );
+             ResultSet rs = pst.executeQuery();) {
+            employees = new ArrayList<>();
+            Employee employee;
+            while ( rs.next() ) {
+                employee = new Employee();
+                employee.setEmpNo( rs.getInt( "empno" ) );
+                employee.setEname( rs.getString( "ename" ) );
+                employee.setJob( rs.getString( "job" ) );
+                employee.setMgr( rs.getInt( "mgr" ) );
+                employee.setHiredate( rs.getDate( "hiredate" ) );
+                employee.setSal( rs.getInt( "sal" ) );
+                employee.setComm( rs.getInt( "comm" ) );
+                employee.setDeptno( rs.getInt( "deptno" ) );
+                employees.add( employee );
             }
-            else {
-                return true;
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
         }
     }
 
