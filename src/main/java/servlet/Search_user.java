ea27f05cdedff74c389c2b1a7d9cd49265306151
md131376st <mona131376st@gmail.com>
2019-05-09 01:00:15 +0430

    added search file

---
+++ src/main/java/servlet/Search_user.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import models.Project;
+import models.Utility;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet("/searchUser/*")
+public class Search_user extends MyServlet{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name =request.getParameter("user_name");
+
+
+
+    }
+}
